ae58a5b603a6245ff7d22fd0a39bf834b3d71147
Sazid Ahamed Sifat <147806245+sazid0411@users.noreply.github.com>
2024-07-27 17:57:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let newGame = document.querySelector("#newGame");
+let winMsg = document.querySelector(".winMassage");
+let msg = document.querySelector("#msg");
+let main = document.querySelector(".main")
+
+let turnO = true;
+
+let winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+const disableBox = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+const enableBox = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = ""
+    }
+}
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation winner is ${winner}`;
+    winMsg.classList.remove("hide");
+}
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                // showWinner(pos1);
+                setTimeout(showWinner(pos1),5000)
+                main.classList.add("hide")
+                disableBox();
+            }
+        }
+    }
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O"
+            turnO = false;
+        } else {
+            box.innerText = "X"
+            turnO = true;
+        }
+        box.disabled = true
+        checkWinner();
+    })
+});
+
+const resetGame = () => {
+    turnO = true;
+    enableBox();
+    winMsg.classList.add("hide")
+}
+
+newGame.addEventListener("click", () => {
+    resetGame();
+    main.classList.remove("hide")
+})
+reset.addEventListener("click", () => {
+    resetGame();
+})
