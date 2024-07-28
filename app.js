76f5bba52fbaa811ed64d9585799e58833ae5467
Sazid Ahamed Sifat <147806245+sazid0411@users.noreply.github.com>
2024-07-28 17:05:17 +0600

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ let reset = document.querySelector("#reset");
 let newGame = document.querySelector("#newGame");
 let winMsg = document.querySelector(".winMassage");
 let msg = document.querySelector("#msg");
-let main = document.querySelector(".main")
+let main = document.querySelector(".main");
+let nowTrun = document.querySelector("#nowTurntext");
+let tapAudio = new Audio("tapp.wav");
+let winAudio = new Audio("win.mp3");
+let drawAudio = new Audio("draw.wav");
+let scoreCountx = document.querySelector("#scorex")
+let scoreCounto = document.querySelector("#scoreo")
+
+let scoreo = 0;
+let scorex = 0;
 
 let turnO = true;
 
@@ -30,36 +39,73 @@ const enableBox = () => {
 }
 const showWinner = (winner) => {
     msg.innerText = `Congratulation winner is ${winner}`;
+    winAudio.play();
     winMsg.classList.remove("hide");
+}
+
+const draw = () => {
+    msg.innerText = `Upss Match Draw`;
+    drawAudio.play();
+    winMsg.classList.remove("hide");
+    main.classList.add("hide")
+
 }
 const checkWinner = () => {
     for (let pattern of winPatterns) {
+
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
 
         if (pos1 != "" && pos2 != "" && pos3 != "") {
             if (pos1 === pos2 && pos2 === pos3) {
-                // showWinner(pos1);
-                setTimeout(showWinner(pos1),5000)
-                main.classList.add("hide")
+                showWinner(pos1);
+                main.classList.add("hide");
+                if ( pos1 === 'X') {
+                    scorex++;
+                    scoreCountx.innerText = `X : ${scorex}`;
+                }
+                else if (pos1 === 'O') {
+                    scoreo++;
+                    scoreCounto.innerText = `O : ${scoreo}`;
+                }
                 disableBox();
             }
         }
     }
 }
-
+const checkDraw = () => {
+    let isDraw = true;
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            isDraw = false;
+            break;
+        }
+    }
+    if (isDraw === true) {
+        draw();
+    } else {
+        checkWinner();
+    }
+};
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
+        tapAudio.play();
         if (turnO) {
             box.innerText = "O"
             turnO = false;
+            if (turnO === false) {
+                nowTrun.innerText = "Now trun of X"
+            }
         } else {
             box.innerText = "X"
             turnO = true;
+            if (turnO) {
+                nowTrun.innerText = "Now trun of O"
+            }
         }
         box.disabled = true
-        checkWinner();
+        checkDraw();
     })
 });
 
@@ -71,8 +117,10 @@ const resetGame = () => {
 
 newGame.addEventListener("click", () => {
     resetGame();
-    main.classList.remove("hide")
+    main.classList.remove("hide");
+    nowTrun.innerText = ""
 })
 reset.addEventListener("click", () => {
     resetGame();
+    nowTrun.innerText = ""
 })
